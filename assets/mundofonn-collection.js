f048af64a2208fed974e8cd96d66495eff312afe
Ecxal Car <ecxalcar0612@gmail.com>
2021-11-15 12:07:09 -0600

    working in faqs

---
+++ assets/mundofonn-collection.js
@@ -0,0 +1,75 @@
+if (Shopify.designMode) {
+    var glider = new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        dots: '.dots',
+        duration: 0.90,
+        itemWidth: 150,
+    
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
+        },
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+                }
+            }
+        ]
+    });
+}
+
+var glider = new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    dots: '.dots',
+    duration: 0.90,
+    itemWidth: 150,
+
+    arrows: {
+        prev: '.glider-prev',
+        next: '.glider-next'
+    },
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4
+            }
+        }
+    ]
+});
