2dbd7b6b1de80784354400b5135479a151cd05bb
Ecxal Car <ecxalcar0612@gmail.com>
2021-11-16 12:20:27 -0600

    working in slider testimonials

---
+++ assets/mundofonn-testimonials.js
@@ -0,0 +1,20 @@
+
+setInterval(function() {
+    var slider = document.getElementById('testimonials-slider');
+    var slidesToShow = slider.dataset.slides;
+    new Glider(document.querySelector('.testimonials-glider'), {
+        slidesToShow: slidesToShow,
+        slidesToScroll: slidesToShow,
+        draggable: false,
+        dots: '.dots-test',
+        duration: 0.90,
+        itemWidth: 150,
+    
+        arrows: {
+            prev: '.glider-prev-testimonials',
+            next: '.glider-next-testimonials'
+        }
+    });
+
+},10) 
+
