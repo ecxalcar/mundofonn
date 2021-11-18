37b2d02534f7922ffa65624bee769ab08b463262
Ecxal Car <ecxalcar0612@gmail.com>
2021-11-18 12:03:46 -0600

    working in default values

--- assets/mundofonn-testimonials.js
+++ assets/mundofonn-testimonials.js
@@ -1,7 +1,7 @@
+function testimonialsSlider() {
+    let slider = document.getElementById('testimonials-slider');
+    let slidesToShow = slider.dataset.slides;
 
-setInterval(function() {
-    var slider = document.getElementById('testimonials-slider');
-    var slidesToShow = slider.dataset.slides;
     new Glider(document.querySelector('.testimonials-glider'), {
         slidesToShow: slidesToShow,
         slidesToScroll: slidesToShow,
@@ -15,5 +15,38 @@ setInterval(function() {
             next: '.glider-next-testimonials'
         }
     });
+}
 
-},10)
+window.addEventListener('load', function() {
+    testimonialsSlider();
+});
+
+let slider = document.getElementById('testimonials-slider');
+let getSectionId = slider.getAttribute('data-sectionId');
+console.log(getSectionId);
+
+document.addEventListener("shopify:section:load", function(event) {
+    if (event.detail.sectionId == getSectionId ) {
+        testimonialsSlider();
+    }
+});
+
+// ===========LOAD SLIDER PREVIEW WITH SET_INTERVAL===========
+// setInterval(function() {
+//     var slider = document.getElementById('testimonials-slider');
+//     var slidesToShow = slider.dataset.slides;
+//     new Glider(document.querySelector('.testimonials-glider'), {
+//         slidesToShow: slidesToShow,
+//         slidesToScroll: slidesToShow,
+//         draggable: false,
+//         dots: '.dots-test',
+//         duration: 0.90,
+//         itemWidth: 150,
+
+//         arrows: {
+//             prev: '.glider-prev-testimonials',
+//             next: '.glider-next-testimonials'
+//         }
+//     });
+
+// },10)
