884058de31445efbbe9736f701b6ae29f7351514
Ecxal Car <ecxalcar0612@gmail.com>
2021-11-24 12:07:35 -0600

    working in table products

--- assets/mundofonn-collection.js
+++ assets/mundofonn-collection.js
@@ -1,36 +1,90 @@
-new Glider(document.querySelector('.glider'), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    draggable: false,
-    dots: '.dots',
-    duration: 0.90,
-    itemWidth: 150,
+function collectionProductSlider() {
 
-    arrows: {
-        prev: '.glider-prev',
-        next: '.glider-next'
-    },
-    responsive: [
-        {
-            breakpoint: 576,
-            settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-            }
+    new Glider(document.querySelector('.glider-products'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: false,
+        dots: '.dots',
+        duration: 0.90,
+        itemWidth: 150,
+    
+        arrows: {
+            prev: '.glider-prev',
+            next: '.glider-next'
         },
-        {
-            breakpoint: 768,
-            settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3
+        responsive: [
+            {
+                breakpoint: 576,
+                settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4
+                }
             }
-        },
-        {
-            breakpoint: 1024,
-            settings: {
-            slidesToShow: 4,
-            slidesToScroll: 4
-            }
-        }
-    ]
+        ]
+    });
+}
+
+window.addEventListener('load', function() {
+    collectionProductSlider();
 });
+
+let sliderProducts = document.getElementById('glider-products-id');
+let getCollectionId = sliderProducts.getAttribute('data-collectionId');
+console.log(getCollectionId);
+
+document.addEventListener("shopify:section:load", function(event) {
+    if (event.detail.sectionId == getCollectionId ) {
+        collectionProductSlider();
+    }
+});
+
+// new Glider(document.querySelector('.glider-products'), {
+//     slidesToShow: 1,
+//     slidesToScroll: 1,
+//     draggable: false,
+//     dots: '.dots',
+//     duration: 0.90,
+//     itemWidth: 150,
+
+//     arrows: {
+//         prev: '.glider-prev',
+//         next: '.glider-next'
+//     },
+//     responsive: [
+//         {
+//             breakpoint: 576,
+//             settings: {
+//             slidesToShow: 2,
+//             slidesToScroll: 2
+//             }
+//         },
+//         {
+//             breakpoint: 768,
+//             settings: {
+//             slidesToShow: 3,
+//             slidesToScroll: 3
+//             }
+//         },
+//         {
+//             breakpoint: 1024,
+//             settings: {
+//             slidesToShow: 4,
+//             slidesToScroll: 4
+//             }
+//         }
+//     ]
+// });
