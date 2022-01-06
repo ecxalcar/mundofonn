dfcdae2caa202dcaab156506272b2485fd3b450a
ecxalcar <ecxalcar0612@gmail.com>
2022-01-06 17:18:26 -0600

    working in modal

--- assets/site-scripts.js
+++ assets/site-scripts.js
@@ -4,6 +4,7 @@ let myModal = new bootstrap.Modal(document.getElementById('productModal'), {})
 modalBtn.forEach(button => {
     button.addEventListener('click', function() {
         let productHandle = button.getAttribute('data-product-handle');
+        let productPrice = button.getAttribute('data-product-price');
         console.log(productHandle);
 
         fetch('/products/' + productHandle + '.js')
@@ -11,7 +12,7 @@ modalBtn.forEach(button => {
             .then(data => {
                     document.querySelector('.product-info img').src = data.featured_image;
                     document.querySelector('.modal-header h3').innerText = data.title;
-                    document.querySelector('.product-info h5').innerText = data.price;
+                    document.querySelector('.product-info h5').innerText = productPrice;
                     document.querySelector('.product-info p').innerHTML = data.description;
                 }
             );
@@ -23,5 +24,12 @@ modalBtn.forEach(button => {
             document.querySelector('.product-info p').innerHTML = '';
         });
     });
+});
 
+var modalClose = document.getElementById('productModal');
+modalClose.addEventListener('hidden.bs.modal', () => {
+    document.querySelector('.product-info img').src = '';
+    document.querySelector('.modal-header h3').innerText = '';
+    document.querySelector('.product-info h5').innerText = '';
+    document.querySelector('.product-info p').innerHTML = '';
 });
