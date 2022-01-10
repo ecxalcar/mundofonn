2fb0afa0ca9139ad0b2bc89d87ba48e82c7cdea1
Ecxal Car <ecxalcar0612@gmail.com>
2022-01-10 12:07:16 -0600

    working in modal car

--- assets/site-scripts.js
+++ assets/site-scripts.js
@@ -1,5 +1,5 @@
 let modalBtn = document.querySelectorAll('.product-modal-btn');
-let myModal = new bootstrap.Modal(document.getElementById('productModal'), {})
+let myModal = new bootstrap.Modal(document.getElementById('productModal'), {});
 
 modalBtn.forEach(button => {
     button.addEventListener('click', function() {
@@ -14,6 +14,12 @@ modalBtn.forEach(button => {
                     document.querySelector('.modal-header h3').innerText = data.title;
                     document.querySelector('.product-info h5').innerText = productPrice;
                     document.querySelector('.product-info p').innerHTML = data.description;
+                    
+                    let productVariantSelected = document.querySelector('#productOption');
+                    productVariantSelected.innerHTML = '';
+                    data.variants.forEach(variant => {
+                        productVariantSelected.options[productVariantSelected.options.length] = new Option(variant.name, variant.id)
+                    });
                 }
             );
         myModal.show();
@@ -32,4 +38,57 @@ modalClose.addEventListener('hidden.bs.modal', () => {
     document.querySelector('.modal-header h3').innerText = '';
     document.querySelector('.product-info h5').innerText = '';
     document.querySelector('.product-info p').innerHTML = '';
+});
+
+let productFormModal = document.querySelector('#productFormModal');
+let productForm = document.querySelector('#productFormModal');
+
+productForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let productVariantSelected = document.querySelector('#productOption').value;
+    let productQuantitySelected = document.querySelector('#productQuantity').value;
+
+    let formData = {
+        'items': [
+            {
+                "id": productVariantSelected,
+                "quantity": productQuantitySelected
+            }
+        ]
+    }
+    console.log(formData);
+
+    fetch('/cart/add.js', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => {
+        if (response.status == 200) {   // *** This can be just `if (response.ok) {`
+            console.log(response);      // *** This is premature
+            let alertSuccess = document.querySelector('.alert');
+            alertSuccess.addClassList('alert-success');
+            alertSuccess.innerHTML = 'Product Added To Cart';
+            alertSuccess.style.display = 'block';
+            setTimeout(function(){
+                alertSuccess.style.display = "none"; 
+            }, 5000);
+            return response.json();
+        }
+        // return response.json();
+
+    })
+    .catch((error) => {
+        let alertSuccess = document.querySelector('.alert');
+            alertSuccess.addClassList('alert-danger');
+            alertSuccess.innerHTML = 'There was an error. You can try again';
+            alertSuccess.style.display = 'block';
+            setTimeout(function(){
+                alertSuccess.style.display = "none"; 
+            }, 5000);
+        console.error('Error:', error);
+        
+    });
 });
